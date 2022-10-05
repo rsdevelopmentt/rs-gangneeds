ea5aa281f8948463ee8763a66855c7d83b2dc425
RS Dev <84734782+rsdevelopmentt@users.noreply.github.com>
2022-10-05 03:22:10 +0300

    Delete rs-gangneeds directory

--- rs-gangneeds/rsgangneeds.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `gangstash` (
-  `gangname` varchar(50) NOT NULL DEFAULT '''''',
-  `pass` int(4) NOT NULL DEFAULT 1234,
-  PRIMARY KEY (`gangname`),
-  UNIQUE KEY `gangname` (`gangname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
